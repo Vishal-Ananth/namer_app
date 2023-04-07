53fcd8e6d903eef69691a94ec00d63660f26ce9f
Vishal Ananth J <imvishal2002@gmail.com>
2023-04-07 19:13:10 +0530

    black screen issue solved

--- lib/main.dart
+++ lib/main.dart
@@ -29,18 +29,9 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
   void getNext() {
     current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favourites = <WordPair>[];
-  void updateFavourite() {
-    if (favourites.contains(current)) {
-      favourites.remove(current);
-    } else {
-      favourites.add(current);
-    }
     notifyListeners();
   }
 }
@@ -51,58 +42,6 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
-    IconData icon;
-    if (appState.favourites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_outline_outlined;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Text('A random Awesome idea:'),
-          BigCard(pair: pair),
-          SizedBox(height: 25),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                  onPressed: () {
-                    appState.updateFavourite();
-                  },
-                  icon: Icon(icon),
-                  label: Text('Like')),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favourites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_outline_outlined;
-    }
-
     return Scaffold(
       body: Center(
         child: Column(
@@ -110,25 +49,12 @@ class GeneratorPage extends StatelessWidget {
           children: [
             // Text('A random Awesome idea:'),
             BigCard(pair: pair),
-            SizedBox(height: 25),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                    onPressed: () {
-                      appState.updateFavourite();
-                    },
-                    icon: Icon(icon),
-                    label: Text('Like')),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    appState.getNext();
-                  },
-                  child: Text('Next'),
-                ),
-              ],
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
             ),
           ],
         ),
